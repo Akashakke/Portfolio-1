ce1027e2d407d95f8efeb5167fb0b05b8a09400c
Akash praveen kumar R <rakashpraveen@karunya.edu.in>
2023-01-09 08:14:36 +0530

    phone

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,13 +12,13 @@ const Footer = () => {
       <div className="f-content">
         <div className="mailid" style={{color:'black'}}>akash.akke4@gmail.com</div>
         <div>
-        <a style={{ padding: "20px" }} href="tel:9047346659">
+      {/*  <a style={{ padding: "20px" }} href="tel:9047346659">
           <img
             src="https://cdn.iconscout.com/icon/premium/png-256-thumb/phone-173-103873.png"
             width="40px"
             
-          />
-        </a>
+  />
+        </a>*/}
         <a style={{ padding: "20px" }} href="mailto:rakashpraveen@karunya.edu.in">
           <img
             src="https://cdn.iconscout.com/icon/free/png-256/gmail-2981844-2476484.png"
